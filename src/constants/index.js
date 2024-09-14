2cf9abdf3fc47755ce070c97f88d22924fd80e56
arpit080 <121251629+arpit080@users.noreply.github.com>
2024-09-14 21:41:46 +0530

    files added

---
+++ src/constants/index.js
@@ -0,0 +1,258 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Anmol Kashyap',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review1.png',
+    review:'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Ayush Pandey',
+    position: 'Founder of TechGear Shop',
+    img: 'assets/review2.png',
+    review:'Arpit’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'Rahul Sharma',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Naba Alam',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:'Arpit was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: '3-d-car-racing-game - By HTML, CSS and JavaScript',
+    desc: 'Experience the thrill of a 3D car game built with HTML, CSS, and JavaScript. Race through dynamic environments, dodge obstacles, and compete for the best time. Enjoy seamless controls and stunning graphics for an immersive, web-based driving adventure.',
+    subdesc:'Join Arpit Singh Yadav s 3D car game, crafted with HTML, CSS, and JavaScript. Navigate challenging tracks and experience high-speed action in your browser.',
+    href: 'https://3-d-car-racing-game.vercel.app/',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'HTML',
+        path: '/assets/HTML.png',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        path: 'assets/CSS.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JavaScript.png',
+      },
+    ],
+  },
+  {
+    title: 'Portfolio - By React JS',
+    desc: 'Explore a dynamic CS engineer portfolio built with React.js, Tailwind CSS, and Next.js. This sleek, responsive site showcases projects, skills, and experience, offering smooth navigation and fast loading. Perfect for highlighting technical expertise and creative problem-solving.',
+    subdesc:'With LiveDoc, Discover Arpit Singh Yadav CS engineer portfolio, crafted with React.js, Next.js, and Tailwind CSS, featuring innovative projects and a modern, responsive design."',
+    href: 'https://portfolio-5-0-theta.vercel.app/',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Grilli Resturent -By HTML, CSS and JavaScript',
+    desc: 'Grilli Restaurant website, crafted with HTML, CSS, and JavaScript, offers an elegant and user-friendly design. Discover the menu, book a table, and explore a vibrant dining experience through seamless navigation, stunning visuals, and interactive elements that engage visitors.',
+    subdesc:'Experience Grilli Restaurant website by Arpit Singh Yadav, built with HTML, CSS, and JavaScript, featuring a sleek design, interactive elements, and easy navigation',
+    href: 'https://small-cafe-website.vercel.app/',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'HTML',
+        path: '/assets/HTML.png',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        path: 'assets/CSS.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JavaScript.png',
+      },
+    ],
+  },
+  {
+    title: 'Flappy Bird Game - By HTML, CSS and JavaScript',
+    desc: 'Enjoy the classic Flappy Bird game, recreated with HTML, CSS, and JavaScript. Test your reflexes as you navigate a bird through challenging obstacles. With smooth animations and engaging gameplay, this browser-based version brings back the addictive fun of the original.',
+    subdesc:'Play Arpit Singh Yadav version of Flappy Bird, built with HTML, CSS, and JavaScript, featuring smooth animations and challenging gameplay in a browser-friendly format',
+    href: 'https://flappy-bird-by-html.vercel.app/',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'HTML',
+        path: '/assets/HTML.png',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        path: 'assets/CSS.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JavaScript.png',
+      },
+    ],
+  },
+  {
+    title: 'Calling Jadoo - By HTML, CSS and JavaScript',
+    desc: 'Calling Jadoo is an innovative software crafted with HTML, CSS, and JavaScript, designed for seamless communication. It offers an intuitive interface, high-quality calls, and reliable connections, enhancing your communication experience with smooth functionality and a user-friendly design.',
+    subdesc:'Discover Calling Jadoo by Arpit Singh Yadav, a software built with HTML, CSS, and JavaScript, offering intuitive design and reliable communication for an enhanced user experience.',
+    href: 'https://calling-jadoo-in-2024-lyart.vercel.app/',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'HTML',
+        path: '/assets/HTML.png',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        path: 'assets/CSS.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JavaScript.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Framer',
+    pos: 'Lead Web Developer',
+    duration: '2022 - Present',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Figma',
+    pos: 'Web Developer',
+    duration: '2020 - 2022',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+    icon: '/assets/figma.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Notion',
+    pos: 'Junior Web Developer',
+    duration: '2019 - 2020',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,136 @@
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
+
+import useAlert from '../hooks/useAlert.js';
+import Alert from '../components/Alert.jsx';
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+
+    const {name, value} = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    //1UMpvyZwJvMaeyd8Z
+    //template_1dxc2pj
+    //service_51mn5jp
+
+    emailjs
+      .send(
+        'service_t820cwc',
+        'template_851lkmc',
+        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Arpit Singh Yadav",
+          from_email: form.email,
+          to_email: "arpitsinghyadav56@gmail.com",
+          message: form.message,
+        },
+        'BvPwXu04zpXfVSz55'
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+
+  }
+
+  return (
+    <section className="c-space my-20" id="contact">
+      {alert.show && <Alert {...alert} />}
+
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+
+        <div className="contact-container">
+          <h3 className="head-text">Let's talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
+            life, I’m here to help.
+          </p>
+
+          <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., Arpit Singh Yadav"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Email address</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., arpitsinghyadav56@gmail.com"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Share your thoughts or inquiries..."
+              />
+            </label>
+
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? 'Sending...' : 'Send Message'}
+
+              <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
